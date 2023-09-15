a9c4d00aaf94f5fa8a4f2f4fc72381a8f561524b
Enailly <129402483+Enailly@users.noreply.github.com>
2023-09-15 12:08:21 -0300

    Add files via upload

---
+++ ciencia-da-computacao/teste.py
@@ -0,0 +1,10 @@
+#variaveis
+#print('olá mundo!')
+#idade = 18; 
+#print(idade)
+#velocidade_Maxima = 240 
+#print(velocidade_Maxima)
+#frase_De_Boas_Vindas = "olá, seja bem-vindo(a)!" #string
+#print(frase_De_Boas_Vindas)
+posso_Entrar_Em_Contato = False #Bolenos
+print(posso_Entrar_Em_Contato)
